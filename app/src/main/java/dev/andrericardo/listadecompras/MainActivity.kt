509d438d920a613f2043798a7b1e31c9eb3939e0
andrericardoweb <andrericardoweb@gmail.com>
2022-09-01 16:28:20 -0300

    config viewBinding and include dependencies

--- app/src/main/java/dev/andrericardo/listadecompras/MainActivity.kt
+++ app/src/main/java/dev/andrericardo/listadecompras/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.andrericardo.listadecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.andrericardo.listadecompras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }

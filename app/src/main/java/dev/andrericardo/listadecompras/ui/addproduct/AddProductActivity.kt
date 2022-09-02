43943e068c5574874a7b3b3deae65e63f434f279
Andre Ricardo <andrericardoweb@gmail.com>
2022-09-02 13:32:17 -0300

    Config viewBinding

--- app/src/main/java/dev/andrericardo/listadecompras/ui/addproduct/AddProductActivity.kt
+++ app/src/main/java/dev/andrericardo/listadecompras/ui/addproduct/AddProductActivity.kt
@@ -2,11 +2,16 @@ package dev.andrericardo.listadecompras.ui.addproduct
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dev.andrericardo.listadecompras.R
+import dev.andrericardo.listadecompras.databinding.ActivityAddProductBinding
 
 class AddProductActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_product)
+        binding = ActivityAddProductBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
